34216ab98eb1483428c5606282e4b7b838344207
EdsonSMartins <smartins.edson@gmail.com>
2024-04-19 14:12:18 +0000

    create base template

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def home(request):
+    return render(request, 'templates/base.html')
+
--- cheesecake_shop/urls.py
+++ cheesecake_shop/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from booking.views import say_hello
+from booking.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
+    path('', home, name='home')
 ]
