b8fbe554d2b6a56101b687368b85164ba9525297
EdsonSMartins <smartins.edson@gmail.com>
2024-04-22 12:12:22 +0000

    Set up index.html and linkcss file

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
+from django.views.generic.edit import CreateView
 
 # Create your views here.
-def home(request):
-    return render(request, 'templates/base.html')
+#def home_view(request):
+#   return render(request,'booking/index.html')
+    
 
+class Home(generic.DetailView):
+    """
+    Renders the Index page in the browser
+    """
+    def get(self, request):
+        return render(request, 'index.html')
